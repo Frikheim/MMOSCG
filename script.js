f148f6eacf7e76cdb6c88b4750139859ba67e4c0
HeddaMarie <43143780+HeddaMarie@users.noreply.github.com>
2018-09-28 20:19:22 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,14 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0;
+bildegalleri();
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+function bildegalleri() {
+    var i;
+    var hentBilder = document.getElementsByClassName("bilde");
+    for (i = 0; i < hentBilder.length; i++) {
+      hentBilder[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > hentBilder.length) {slideIndex = 1} 
+    hentBilder[slideIndex-1].style.display = "block"; 
+    setTimeout(bildegalleri, 3000); 
 }
